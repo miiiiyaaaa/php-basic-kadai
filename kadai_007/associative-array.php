bfa39ec2b0c859a740945692b6879178288073ac
miiiiyaaaa <pissenlit.2624@gmail.com>
2024-09-17 11:59:36 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編007課題</title>
+</head>
+
+<body>
+
+    <?php
+    $vege = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+    print_r($vege)
+    ?>
+
+</body>
+
