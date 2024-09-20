9ac4879bebbe1a0df4e7352b2578ea44378841f3
miiiiyaaaa <pissenlit.2624@gmail.com>
2024-09-20 13:19:15 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編課題011</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $vege = ['名前' => '玉ねぎ','値段' => 200,'産地' => '北海道'];
+
+        foreach ($vege as $key => $value) {
+            echo "{$key}:{$value}<br>";
+        }
+        ?>
+    </p>
+</body>
+
+</html>
